5812b0bcdab55dc246068cf11259f407ddf9827e
vu2005 <vunnth2307024@fpt.edu.vn>
2023-12-04 05:46:56 +0700

    first commit

---
+++ Day3-Modal/assest/javascript/app.js
@@ -0,0 +1,6 @@
+// khai báo sự kiện
+var clickButton = document.querySelector('.click');
+//truyền dữ liệu r khai báo ra ngoài
+clickButton.addEventListener('click', function (){
+    alert('Hello cậu => ');
+})
---
+++ Day8-RegisterForm/assest/javascript/app.js
@@ -0,0 +1,96 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.querySelector('form');
+    const username = document.getElementById('username');
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+    const password2 = document.getElementById('password2');
+    const clickSubmit = document.getElementById('clickSubmit');
+
+    clickSubmit.addEventListener('click', function (e) {
+        e.preventDefault();
+        if (checkRequired([username, email, password, password2])) {
+            const isPasswordValid = checkLength(password, 6, 25);
+            const arePasswordsMatching = checkPasswordsMatch(password, password2);
+            const isEmailValid = checkEmail(email);
+
+            if (isPasswordValid && arePasswordsMatching && isEmailValid) {
+                // Hiển thị thông báo khi đăng ký thành công
+                alert('Đăng Ký Thành Công');
+            }
+        }
+    });
+
+    function checkRequired(inputArr) {
+        let isRequired = true;
+        inputArr.forEach(function (input) {
+            if (input.value.trim() === '') {
+                showError(input, `${getFieldName(input)} is required`);
+                isRequired = false;
+            } else {
+                showSuccess(input);
+            }
+        });
+
+        return isRequired;
+    }
+
+    function showError(input, message) {
+        const formControl = input.parentElement;
+        formControl.className = 'form-control error';
+        const small = formControl.querySelector('small');
+        small.innerText = message;
+    }
+
+    function showSuccess(input) {
+        const formControl = input.parentElement;
+        formControl.className = 'form-control success';
+        const small = formControl.querySelector('small');
+        small.innerText = '';
+    }
+
+    function checkEmail(input) {
+        const re =
+            /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        if (re.test(input.value.trim())) {
+            showSuccess(input);
+            return true;
+        } else {
+            showError(input, 'Email is not valid');
+            return false;
+        }
+    }
+
+    function checkLength(input, min, max) {
+        if (input.value.length < min) {
+            showError(
+                input,
+                `${getFieldName(input)} must be at least ${min} characters`
+            );
+            return false;
+        } else if (input.value.length > max) {
+            showError(
+                input,
+                `${getFieldName(input)} must be less than ${max} characters`
+            );
+            return false;
+        } else {
+            showSuccess(input);
+            return true;
+        }
+    }
+
+    function checkPasswordsMatch(input1, input2) {
+        if (input1.value !== input2.value) {
+            showError(input2, 'Passwords do not match');
+            return false;
+        } else {
+            showSuccess(input2);
+            return true;
+        }
+    }
+
+    function getFieldName(input) {
+        return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+    }
+});
